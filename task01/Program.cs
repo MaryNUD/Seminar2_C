9c3d63d84d60c6c3327700808a6c8c56485656e1
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-05 18:14:28 +0300

    add task 01 array

---
+++ task01/Program.cs
@@ -0,0 +1,4 @@
+﻿int [] array = new int [8];
+for (int i = 0; i < array.Length; i++ )
+    array [i] = new Random().Next(-10,11); // [-10, 10]
+Console.WriteLine("[" + string.Join(", ", array) + "]"); // make a string from our array
