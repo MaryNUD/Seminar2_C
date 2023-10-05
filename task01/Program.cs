40bdad48e87a2716e54fbdb7c53ad60bcf4ffcbe
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-05 18:25:15 +0300

    new version of task 01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,4 +1,10 @@
 ﻿int [] array = new int [8];
-for (int i = 0; i < array.Length; i++ )
-    array [i] = new Random().Next(-10,11); // [-10, 10]
-Console.WriteLine("[" + string.Join(", ", array) + "]"); // make a string from our array
+for (int i = 0; i < array.Length; i++)
+  array [i] = new Random().Next(-10,11); // [-10, 10]
+Console.WriteLine($"[{string.Join(", ", array)}]"); // make a string from our array
+
+//string name = "Maryia";
+//int age = 21;
+//string city = "Minsk";
+
+//Console.WriteLine($"My name is {name}. I am {age} years old. I am from {city}.");
